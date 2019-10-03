d0c680757f73a5910d38c694d757210538bb9800
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-10-03 16:09:48 -0400

    revised detail pages to have linebreaks. Added Listview for homepage.

--- teambuilding/teambuilding/views.py
+++ teambuilding/teambuilding/views.py
@@ -1,5 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+
+from projects.models import Project
 
 
-class Home(TemplateView):
+class Home(ListView):
+    model = Project
     template_name = 'index.html'
+
+    def get_queryset(self):
+        filter_value = self.request.GET.get('filter', 'All Needs')
+        if filter_value == 'All Needs':
+            context = Project.objects.all()
+        else:
+            context = Project.objects.filter(
+                position__title__icontains=filter_value[0:3]
+            )
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        context['filter'] = self.request.GET.get('filter')
+        return context
