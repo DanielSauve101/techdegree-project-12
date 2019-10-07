b401726a4a1c5cf43282952ed40263043198fdf6
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-10-07 14:12:56 -0400

    added ListApplicationsView

--- teambuilding/applications/urls.py
+++ teambuilding/applications/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from .views import ListApplicationsView
 
+app_name = 'applications'
 urlpatterns = [
-    
+    path('', ListApplicationsView.as_view(), name="applications-list"),
 ]
